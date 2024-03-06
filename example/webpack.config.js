93681dd629738405c799ba301087f26e8d06a949
fouadsalkini <fouad@sync.com.lb>
2024-03-06 13:54:10 +0200

    changed webpack config

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -5,6 +5,11 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: "./src/index.js",
   mode: "production",
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+},
   module: {
     rules: [
       {
